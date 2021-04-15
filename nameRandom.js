45556639c72e5a90499ef06acb8f02f35582fc5f
Prodooff <61739059+Prodooff@users.noreply.github.com>
2021-04-15 11:45:23 +0300

    Add files via upload

---
+++ nameRandom.js
@@ -0,0 +1,276 @@
+function getRandName(){
+    let name = document.getElementById("test2")
+    let randName = Math.floor(Math.random() * 44)
+    console.log(randName)
+    let genderSelect = document.getElementById("gender")
+    let genderSelectValue = genderSelect.value
+
+    if(genderSelectValue=="Мужское"){
+            switch(randName){
+            case 1:
+            name.innerHTML = "Игорь"
+            break;
+            case 2:
+            name.innerHTML = "Николай"
+            break;
+            case 3:
+            name.innerHTML = "Владимир"
+            break;
+            case 4:
+            name.innerHTML = "Владислав"
+            break;
+            case 5:
+            name.innerHTML = "Назар"
+            break;
+            case 6:
+            name.innerHTML = "Александр"
+            break;
+            case 7:
+            name.innerHTML = "Матвей"
+            break;
+            case 8:
+            name.innerHTML = "Ростислав"
+            break;
+            case 9:
+            name.innerHTML = "Егор"
+            break;
+            case 10:
+            name.innerHTML = "Михаил"
+            break;
+            case 11:
+            name.innerHTML = "Ярослав"
+            break;
+            case 12:
+            name.innerHTML = "Святослав"
+            break;
+            case 13:
+            name.innerHTML = "Илья"
+            break;
+            case 14:
+            name.innerHTML = "Леонид"
+            break;
+            case 15:
+            name.innerHTML = "Чепухин"
+            break;
+            case 16:
+            name.innerHTML = "Артем"
+            break;
+            case 17:
+            name.innerHTML = "Родион"
+            break;
+            case 18:
+            name.innerHTML = "Денис"
+            break;
+            case 19:
+            name.innerHTML = "Алексей"
+            break;
+            case 20:
+            name.innerHTML = "Кирилл"
+            break;
+            case 21:
+            name.innerHTML = "Евгений"
+            break;
+            case 22:
+            name.innerHTML = "Даниил"
+            break;
+            case 23:
+            name.innerHTML = "Павел"
+            break;
+            case 24:
+            name.innerHTML = "Константин"
+            break;
+            case 25:
+            name.innerHTML = "Валерий"
+            break;
+            case 26:
+            name.innerHTML = "Олег"
+            break;
+            case 27:
+            name.innerHTML = "Юрий"
+            break;
+            case 28:
+            name.innerHTML = "Руслан"
+            break;
+            case 29:
+            name.innerHTML = "Семен"
+            break;
+            case 30:
+            name.innerHTML = "Сергей"
+            break;
+            case 31:
+            name.innerHTML = "Геннадий"
+            break;
+            case 32:
+            name.innerHTML = "Глеб"
+            break;
+            case 33:
+            name.innerHTML = "Давид"
+            break;
+            case 34:
+            name.innerHTML = "Федор"
+            break;
+            case 35:
+            name.innerHTML = "Ян"
+            break;
+            case 36:
+            name.innerHTML = "Кира"
+            break;
+            case 37:
+            name.innerHTML = "Роман"
+            break;
+            case 38:
+            name.innerHTML = "Вадим"
+            break;
+            case 39:
+            name.innerHTML = "Август"
+            break;
+            case 40:
+            name.innerHTML = "Альберт"
+            break;
+            case 41:
+            name.innerHTML = "Григорий"
+            break;
+            case 42:
+            name.innerHTML = "Вальдемар"
+            break;
+            case 43:
+            name.innerHTML = "Владлен"
+            break;
+        }
+    }
+    else if(genderSelectValue=="Женское"){
+            switch(randName){
+            case 1:
+            name.innerHTML = "Кристина"
+            break;
+            case 2:
+            name.innerHTML = "Милана"
+            break;
+            case 3:
+            name.innerHTML = "Алиса"
+            break;
+            case 4:
+            name.innerHTML = "Ольга"
+            break;
+            case 5:
+            name.innerHTML = "Анастасия"
+            break;
+            case 6:
+            name.innerHTML = "Светлана"
+            break;
+            case 7:
+            name.innerHTML = "Виктория"
+            break;
+            case 8:
+            name.innerHTML = "София"
+            break;
+            case 9:
+            name.innerHTML = "Софья"
+            break;
+            case 10:
+            name.innerHTML = "Соня"
+            break;
+            case 11:
+            name.innerHTML = "Екатерина"
+            break;
+            case 12:
+            name.innerHTML = "Дарья"
+            break;
+            case 13:
+            name.innerHTML = "Евгения"
+            break;
+            case 14:
+            name.innerHTML = "Полина"
+            break;
+            case 15:
+            name.innerHTML = "Алена"
+            break;
+            case 16:
+            name.innerHTML = "Яна"
+            break;
+            case 17:
+            name.innerHTML = "Марии"
+            break;
+            case 18:
+            name.innerHTML = "Галина"
+            break;
+            case 19:
+            name.innerHTML = "Елена"
+            break;
+            case 20:
+            name.innerHTML = "Динара"
+            break;
+            case 21:
+            name.innerHTML = "Ева"
+            break;
+            case 22:
+            name.innerHTML = "Владислава"
+            break;
+            case 23:
+            name.innerHTML = "Жанна"
+            break;
+            case 24:
+            name.innerHTML = "Маргарита"
+            break;
+            case 25:
+            name.innerHTML = "Карина"
+            break;
+            case 26:
+            name.innerHTML = "Ирина"
+            break;
+            case 27:
+            name.innerHTML = "Людмила"
+            break;
+            case 28:
+            name.innerHTML = "Любовь"
+            break;
+            case 29:
+            name.innerHTML = "Руслана"
+            break;
+            case 30:
+            name.innerHTML = "Олеся"
+            break;
+            case 31:
+            name.innerHTML = "Алеся"
+            break;
+            case 32:
+            name.innerHTML = "Милена"
+            break;
+            case 33:
+            name.innerHTML = "Марта"
+            break;
+            case 34:
+            name.innerHTML = "Надежда"
+            break;
+            case 35:
+            name.innerHTML = "Нина"
+            break;
+            case 36:
+            name.innerHTML = "Оксана"
+            break;
+            case 37:
+            name.innerHTML = "Снежана"
+            break;
+            case 38:
+            name.innerHTML = "Таисия"
+            break;
+            case 39:
+            name.innerHTML = "Тамара"
+            break;
+            case 40:
+            name.innerHTML = "Фаина"
+            break;
+            case 41:
+            name.innerHTML = "Эмма"
+            break;
+            case 42:
+            name.innerHTML = "Юлиана"
+            break;
+            case 43:
+            name.innerHTML = "Эльза"
+            break;
+
+
+        }
+    }
+}
---
+++ numRandom.js
@@ -0,0 +1,16 @@
+function genRandomNum(){
+    let randBlockNum1 = document.getElementById("randblock1")
+    let randNumMin = Number(randBlockNum1.value)
+    let randBlockNum2 = document.getElementById("randblock2")
+    let randNumMax = Number(randBlockNum2.value)
+
+    if(randNumMin>randNumMax){
+        alert("Первое число не может быть больше второго ")     // Проверка на то, что второй атрибут больше первого
+    }
+
+    if(randNumMax>randNumMin){
+        let test = document.getElementById("test")
+        test.innerHTML = Math.floor(Math.random() * randNumMax) + randNumMin
+    }
+    
+}
