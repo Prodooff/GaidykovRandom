2dcf49acd439d911b41e0801efcb2ce69b5e41be
Prodooff <61739059+Prodooff@users.noreply.github.com>
2021-04-20 11:49:20 +0300

    + normal logic for date

--- dateRandom.js
+++ dateRandom.js
@@ -1,10 +1,54 @@
-start = new Date(2001, 0, 1)
-end = new Date()
-
 function getRandomDate() {
+    let getStartYear = document.getElementById("InpStartYear").value
+    let getStartMonth = document.getElementById("InpStartMonth").value
+    let getStartDay = document.getElementById("InpStartDay").value
+    let getEndYear = document.getElementById("InpEndYear").value
+    let getEndMonth = document.getElementById("InpEndMonth").value
+    let getEndDay = document.getElementById("InpEndDay").value
+    start = new Date(getStartYear,getStartMonth,getStartDay)
+    end = new Date(getEndYear,getEndMonth,getEndDay)
     let randDate = document.getElementById("date")
     let randDateResult = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+    let randDateMonth = randDateResult.getMonth()  
+    switch (randDateMonth) {
+        case 0:
+            a =( 'Январь' );
+            break;
+        case 1:
+            a =( 'Февраль' );
+            break;
+        case 2:
+            a =( 'Март' );
+            break;
+        case 3:
+            a =( 'Апрель' );
+            break;
+        case 4:
+            a =( 'Май' );
+            break;
+        case 5:
+            a =( 'Июнь' );
+            break;
+        case 6:
+            a =( 'Июль' );
+            break;
+        case 7:
+            a =( 'Август' );
+            break;
+        case 8:
+            a =( 'Сентябрь' );
+            break;
+        case 9:
+            a =( 'Октябрь' );
+            break;
+        case 10:
+            a =( 'Ноябрь' );
+            break;    
+        case 11:
+            a =( 'Декабрь' );
+            break;                     
+    }
     // let randDateCompileResult = randDateResult.toLocaleDateString()
-    let randDateCompileResult = "Год: " + randDateResult.getFullYear() + " Месяц: " + randDateResult.getMonth() + " День: " + randDateResult.getDate()
+    let randDateCompileResult = "Год: " + randDateResult.getFullYear() + " Месяц: " + a + " День: " + randDateResult.getDate()
     randDate.innerHTML = randDateCompileResult
 }
